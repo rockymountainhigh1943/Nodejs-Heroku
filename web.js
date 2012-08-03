bf4d8ad0bf9db13412448632a52bda5d14a0d620
Jake Love <rockymountainhigh1943@gmail.com>
2012-08-02 23:21:06 -0700

    more changes

--- web.js
+++ web.js
@@ -3,15 +3,6 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response){
-	var html = '<!DOCTYPE HTML>
-	<html lang="en">
-	<head>
-		<title>Week 3 | Node.js with Express</title>
-	</head>
-	<body>
-  		<h1>Hello World!</h1>
-  	</body>
-	</html>';
 	response.send('Hi baby');
 });
 
