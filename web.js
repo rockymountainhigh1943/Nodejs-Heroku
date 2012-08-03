eae73652413839b5d0d67249ed1510f086aee787
Jake Love <rockymountainhigh1943@gmail.com>
2012-08-02 23:27:44 -0700

    please

--- web.js
+++ web.js
@@ -3,15 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response){
-	response.send('<!DOCTYPE HTML>
-<html lang="en">
-<head>
-<title>Week 3 | Node.js with Express</title>
-</head>
-<body>
-	<h1>Hello World!</h1>
-</body>
-</html>');
+	response.send('<!DOCTYPE HTML><html lang="en"><head><title>Week 3 | Node.js with Express</title></head><body><h1>Hello World!</h1></body></html>');
 });
 
 var port = process.env.PORT || 5000;
