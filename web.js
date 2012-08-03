c75ea3469d5bb39031ec22fb54ef6e27ddbefd92
Jake Love <rockymountainhigh1943@gmail.com>
2012-08-02 23:31:41 -0700

    adding user function

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response){
 	response.send('<!DOCTYPE HTML><html lang="en"><head><title>Week 3 | Node.js with Express</title></head><body><h1>Hello World!</h1></body></html>');
 });
 
+app.get('/user/:username', function(request, response){
+	response.send(request.param['username']);
+});
+
 var port = process.env.PORT || 5000;
 
 app.listen(port, function(){
