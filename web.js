7eb800828ae772841cee9cdfae27c061403ac07a
Jake Love <rockymountainhigh1943@gmail.com>
2012-08-02 23:22:58 -0700

    more

--- web.js
+++ web.js
@@ -2,6 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var html = '<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>Week 3 | Node.js with Express</title>
+</head>
+<body>
+	<h1>Hello World!</h1>
+</body>
+</html>';
+
 app.get('/', function(request, response){
 	response.send('Hi baby');
 });
